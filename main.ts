862d7584cf3d869d5ae31e787e23fc2db98d5e1c
Nandhu <nandhumouli1@gmail.com>
2023-12-27 14:11:58 +0530

    project done

---
+++ main.ts
@@ -0,0 +1,48 @@
+  GNU nano 7.2                        main.ts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                  [ New File ]
+^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
+  GNU nano 7.2                        main.ts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                  [ New File ]
+^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
